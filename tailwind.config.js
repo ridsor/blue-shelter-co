97c86c7e95f5cc62eeec33420f68595445f312ec
RYAN SYUKUR <ridsorgamerz@gmail.com>
2023-05-12 20:09:13 +0900

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  darkMode: "class",
+  content: ["./public/**/*.{html,js}", "./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
