50f3c64f964e8e4b2fabe55e1e0edf6cbe7a66be
RYAN SYUKUR <ridsorgamerz@gmail.com>
2023-05-22 17:37:58 +0900

    memperbaiki semua halaman dan menambah aos dan animate.css

--- public/js/main.js
+++ public/js/main.js
@@ -34,26 +34,34 @@ window.onscroll = function () {
 };
 
 // Beranda
-$(".hero-slick").slick({
-  arrows: false,
-  dots: false,
-  infinite: true,
-  autoplay: true,
-  autoplaySpeed: 4000,
-});
+try {
+  $(".hero-slick").slick({
+    arrows: false,
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+  });
+} catch (e) {
+  console.error(e);
+}
 
-const btnSelengkapnya = document.getElementById("btn_selengkapnya");
-btnSelengkapnya.addEventListener("click", function () {
-  const wordHeight =
-    btnSelengkapnya.previousElementSibling.children[0].offsetHeight;
-  console.log(wordHeight);
-  if (btnSelengkapnya.innerText == "TAMPILKAN LEBIH BANYAK") {
-    btnSelengkapnya.previousElementSibling.style.height = wordHeight + "px";
-    btnSelengkapnya.previousElementSibling.classList.add("before:hidden");
-    btnSelengkapnya.innerText = "TAMPILKAN LEBIH SEDIKIT";
-  } else {
-    btnSelengkapnya.previousElementSibling.style.height = "12rem";
-    btnSelengkapnya.previousElementSibling.classList.remove("before:hidden");
-    btnSelengkapnya.innerText = "TAMPILKAN LEBIH BANYAK";
-  }
-});
+try {
+  const btnSelengkapnya = document.getElementById("btn_selengkapnya");
+  btnSelengkapnya.addEventListener("click", function () {
+    const wordHeight =
+      btnSelengkapnya.previousElementSibling.children[0].offsetHeight;
+    console.log(wordHeight);
+    if (btnSelengkapnya.innerText == "TAMPILKAN LEBIH BANYAK") {
+      btnSelengkapnya.previousElementSibling.style.height = wordHeight + "px";
+      btnSelengkapnya.previousElementSibling.classList.add("before:hidden");
+      btnSelengkapnya.innerText = "TAMPILKAN LEBIH SEDIKIT";
+    } else {
+      btnSelengkapnya.previousElementSibling.style.height = "12rem";
+      btnSelengkapnya.previousElementSibling.classList.remove("before:hidden");
+      btnSelengkapnya.innerText = "TAMPILKAN LEBIH BANYAK";
+    }
+  });
+} catch (e) {
+  console.error(e);
+}
